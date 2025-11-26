24eb3b812f3839695b5e3fc3a0173b76c5a9265a
AlejoX0 <mvb049@gmail.com>
2025-11-25 20:51:13 -0500

    jum

---
+++ app/build/generated/data_binding_trigger/debug/com/zenit/digital/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.zenit.digital;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ app/src/main/java/com/zenit/digital/ui/login/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.zenit.digital.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.zenit.digital.R
+import com.zenit.digital.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
